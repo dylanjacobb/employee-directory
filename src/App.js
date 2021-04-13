502a65547895a5d36fd741503c7c36c623101da6
Dylan Jacob <dylanjacob.re@gmail.com>
2021-04-13 09:57:40 -0400

    added json stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Search from "./Pages/Search";
-// import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <Wrapper>
           <Route exact path="/" component={Search} />
         </Wrapper>
-        {/* <Footer /> */}
       </div>
     </Router>
   );
--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./style.css";
 
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
 function SearchForm(props) {
   return (
     <form className="search">
